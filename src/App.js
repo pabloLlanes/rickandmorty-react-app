b4f2403a7346fe5d668c0faefe228f3e73414890
Pablo Llanes <41196999+pabloLlanes@users.noreply.github.com>
2021-08-10 14:51:09 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="h-32 bg-black flex justify-center items-center">
+        <h3 className="text-3xl text-white">Hola</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
